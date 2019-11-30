a4dcfec0039d828ebd7ccd0df9e4693c2295fd52
m00nkeyD <j.rojaszafra.p@gmail.com>
2019-11-30 14:07:50 +0100

    mergeremedy

---
+++ src/main/java/ejemplo/jrojas/PruebaMaven/Animal.java
@@ -0,0 +1,26 @@
+package ejemplo.jrojas.PruebaMaven;
+
+public class Animal {
+	private String nombre;
+	private int edad;
+	public Animal() {
+		super();
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	@Override
+	public String toString() {
+		return "Animal [nombre=" + nombre + ", edad=" + edad + "]";
+	}
+	
+}
