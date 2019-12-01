0d09f95af1f81e1f79ba5f741ba600845eee5f23
m00nkeyD <j.rojaszafra.p@gmail.com>
2019-12-01 10:51:13 +0100

    Actualización Procedencia

--- src/main/java/ejemplo/jrojas/PruebaMaven/model/Persona.java
+++ src/main/java/ejemplo/jrojas/PruebaMaven/model/Persona.java
@@ -3,24 +3,48 @@ package ejemplo.jrojas.PruebaMaven.model;
 public class Persona {
 	private String nombre;
 	private int edad;
+	private Tipo tipo;
+	private String procedencia;
+
 	public Persona() {
 		super();
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public int getEdad() {
 		return edad;
 	}
+
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
+
+	public Tipo getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(Tipo tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getProcedencia() {
+		return procedencia;
+	}
+
+	public void setProcedencia(String procedencia) {
+		this.procedencia = procedencia;
+	}
+
 	@Override
 	public String toString() {
 		return "Persona [nombre=" + nombre + ", edad=" + edad + "]";
 	}
-	
+
 }
